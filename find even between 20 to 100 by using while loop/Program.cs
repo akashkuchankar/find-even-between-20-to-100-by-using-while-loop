bd667ec92eca7969fd5dac27a6578a21ac8323eb
akashkuchankar <kuchankarakash@gmail.com>
2022-10-05 01:20:49 -0700

    Add project files.

---
+++ find even between 20 to 100 by using while loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_even_between_20_to_100_by_using_while_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int i = 20;
+            while(i<=100)
+            {
+                if(i%2!=0)
+                    Console.WriteLine(i);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
